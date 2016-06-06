062c6a27d7d5e6739c5f3fd04aa5f03041791a16
Will Allstead <willallstead@icloud.com>
2016-06-06 16:44:27 -0700

    Finishing up the replacement to SKPixelButtonNode.

--- Cat World/SKPixel.swift
+++ Cat World/SKPixel.swift
@@ -47,12 +47,143 @@ class SKPixelSpriteNode: SKSpriteNode {
         yScale = oldYScale
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        action?()
+    }
+}
+
+class SKPixelButtonNode: SKPixelSpriteNode {
+    var defaultTexture: SKTexture
+    var activeTexture: SKTexture
+    var downSound: SKAudioNode
+    var upSound: SKAudioNode
+    var text: SKLabelNode?
+    
+    init(textureName: String, text: String? = nil) {
+        let texture = SKTexture(imageNamed: textureName)
+        texture.filteringMode = SKTextureFilteringMode.Nearest
+        let texturePressed = SKTexture(imageNamed: textureName+"_pressed")
+        texturePressed.filteringMode = SKTextureFilteringMode.Nearest
+        self.defaultTexture = texture
+        self.activeTexture = texturePressed
+        self.downSound = SKAudioNode(fileNamed: "button_down.wav")
+        self.upSound = SKAudioNode(fileNamed: "button_up.wav")
+        
+        super.init(textureName: textureName)
+        self.textureName = textureName
+        
+        self.userInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.defaultTexture = aDecoder.decodeObjectForKey("defaulttexture") as! SKTexture
+        self.activeTexture = aDecoder.decodeObjectForKey("activeTexture") as! SKTexture
+        self.downSound = aDecoder.decodeObjectForKey("downSound") as! SKAudioNode
+        self.upSound = aDecoder.decodeObjectForKey("upSound") as! SKAudioNode
+        self.text = aDecoder.decodeObjectForKey("text") as? SKLabelNode
+        super.init(coder: aDecoder)
+        self.userInteractionEnabled = true
+    }
+    
+    override func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.defaultTexture, forKey: "defaultTexture")
+        aCoder.encodeObject(self.activeTexture, forKey: "activeTexture")
+        aCoder.encodeObject(self.downSound, forKey: "downSound")
+        aCoder.encodeObject(self.upSound, forKey: "upSound")
+        aCoder.encodeObject(self.text, forKey: "text")
+        super.encodeWithCoder(aCoder)
+    }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         action?()
     }
-    
 }
+
+//class SKPixelButtonNode: SKPixelSpriteNode {
+//    var defaultTexture: SKTexture
+//    var activeTexture: SKTexture
+//    var downSound: SKAudioNode
+//    var upSound: SKAudioNode
+//    var text: SKLabelNode?
+//
+//    init(textureName: String, text: String?) {
+//        let activeButtonImage = buttonImage+"_pressed"
+//        defaultButton = SKTexture(imageNamed: buttonImage)
+//        defaultButton.filteringMode = SKTextureFilteringMode.Nearest
+//        activeButton = SKTexture(imageNamed: activeButtonImage)
+//        activeButton.filteringMode = SKTextureFilteringMode.Nearest
+//        buttonDown = SKAudioNode(fileNamed: "button_down.wav")
+//        buttonDown.autoplayLooped = false
+//        buttonDown.positional = false
+//        buttonUp = SKAudioNode(fileNamed: "button_up.wav")
+//        buttonUp.autoplayLooped = false
+//        buttonUp.positional = false
+//        action = buttonAction
+//
+//
+//        super.init(textureName: buttonImage, pressAction: {})
+//
+//        userInteractionEnabled = true
+//        self.addChild(buttonDown)
+//        self.addChild(buttonUp)
+//        if (buttonText != nil) {
+//            text = SKLabelNode(fontNamed: "Silkscreen")
+//
+//            text!.text = buttonText
+//            text!.setScale(1/10)
+//            text!.fontSize = 80
+//            text!.fontColor = SKColor.whiteColor()
+//            text!.verticalAlignmentMode = .Center
+//            text!.zPosition = 1
+//            self.addChild(text!)
+//
+//        }
+//    }
+//
+//    required init(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        self.texture = activeButton
+//        buttonDown.runAction(SKAction.play())
+//        if (text != nil) {
+//            text?.position.y = -1
+//        }
+//    }
+//
+//    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        if let touch = touches.first {
+//            let location: CGPoint = touch.locationInNode(self.parent!)
+//            if self.containsPoint(location) {
+//                self.texture = activeButton
+//                if (text != nil) {
+//                    text?.position.y = -1
+//                }
+//            } else {
+//                self.texture = defaultButton
+//                if (text != nil) {
+//                    text?.position.y = 0
+//                }
+//            }
+//        }
+//
+//    }
+//
+//    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//
+//        if self.texture == activeButton {
+//            action()
+//            buttonUp.runAction(SKAction.play())
+//        }
+//
+//        if (text != nil) {
+//            text?.position.y = 0
+//        }
+//        self.texture = defaultButton
+//
+//    }
+//}
 
 //class SKPixelSpriteNode: SKSpriteNode {
 //    var textureName: String
@@ -87,9 +218,9 @@ class SKPixelSpriteNode: SKSpriteNode {
 //    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
 //        pressAction()
 //    }
-    
-
-    
+//    
+//
+//    
 //    required convenience init(coder decoder: NSCoder) {
 //        super.init()
 //        self.wallpaper = decoder.decodeObjectForKey("wallpaper") as! SKPixelSpriteNode
@@ -113,94 +244,7 @@ class SKPixelSpriteNode: SKSpriteNode {
 //
 //}
 //
-//class SKPixelButtonNode: SKPixelSpriteNode {
-//    var defaultButton: SKTexture
-//    var activeButton: SKTexture
-//    var buttonDown: SKAudioNode
-//    var buttonUp: SKAudioNode
-//    var text: SKLabelNode?
-//    internal var action: () -> Void
-//    
 //
-//    
-//    init(buttonImage: String, buttonText: String?, buttonAction: () -> Void) {
-//        let activeButtonImage = buttonImage+"_pressed"
-//        defaultButton = SKTexture(imageNamed: buttonImage)
-//        defaultButton.filteringMode = SKTextureFilteringMode.Nearest
-//        activeButton = SKTexture(imageNamed: activeButtonImage)
-//        activeButton.filteringMode = SKTextureFilteringMode.Nearest
-//        buttonDown = SKAudioNode(fileNamed: "button_down.wav")
-//        buttonDown.autoplayLooped = false
-//        buttonDown.positional = false
-//        buttonUp = SKAudioNode(fileNamed: "button_up.wav")
-//        buttonUp.autoplayLooped = false
-//        buttonUp.positional = false
-//        action = buttonAction
-//        
-//        
-//        super.init(textureName: buttonImage, pressAction: {})
-//        
-//        userInteractionEnabled = true
-//        self.addChild(buttonDown)
-//        self.addChild(buttonUp)
-//        if (buttonText != nil) {
-//            text = SKLabelNode(fontNamed: "Silkscreen")
-//            
-//            text!.text = buttonText
-//            text!.setScale(1/10)
-//            text!.fontSize = 80
-//            text!.fontColor = SKColor.whiteColor()
-//            text!.verticalAlignmentMode = .Center
-//            text!.zPosition = 1
-//            self.addChild(text!)
-//            
-//        }
-//    }
-//    
-//    required init(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-//        self.texture = activeButton
-//        buttonDown.runAction(SKAction.play())
-//        if (text != nil) {
-//            text?.position.y = -1
-//        }
-//    }
-//    
-//    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-//        if let touch = touches.first {
-//            let location: CGPoint = touch.locationInNode(self.parent!)
-//            if self.containsPoint(location) {
-//                self.texture = activeButton
-//                if (text != nil) {
-//                    text?.position.y = -1
-//                }
-//            } else {
-//                self.texture = defaultButton
-//                if (text != nil) {
-//                    text?.position.y = 0
-//                }
-//            }
-//        }
-//        
-//    }
-//    
-//    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-//        
-//        if self.texture == activeButton {
-//            action()
-//            buttonUp.runAction(SKAction.play())
-//        }
-//        
-//        if (text != nil) {
-//            text?.position.y = 0
-//        }
-//        self.texture = defaultButton
-//        
-//    }
-//}
 //
 //class SKPixelMenuButtonNode: SKPixelSpriteNode {
 //    var defaultButton: SKTexture
